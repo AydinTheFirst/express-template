4199f964e74cb8ba548b2032f0e6c7b52124ed0e
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-06-26 16:57:42 +0300

    chore: Update dependencies and add middleware for logging requests

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,14 +3,8 @@ import Logger from "@/lib/Logger";
 import mongoose from "mongoose";
 
 (async () => {
-  try {
-    const conn = await mongoose.connect(config.MONGODB_URI!, {
-      dbName: "test",
-    });
-    Logger.success(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      Logger.error(`MongoDB error: ${error.message}`);
-    }
-  }
+  const conn = await mongoose.connect(config.MONGODB_URI!, {
+    dbName: "test",
+  });
+  Logger.success(`MongoDB connected: ${conn.connection.host}`);
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+import "dotenv/config";
+import "@/database";
 import "@/server";
